6fe8c4060b63611114fd6b3e9c886107a7e3b42a
Joonas <KA0Screativity@gmail.com>
2022-07-25 20:29:57 +0300

    Säädä JavaFx toimimaan

---
+++ GoPeli/src/main/java/GoPeli/kayttoliittyma/Kayttoliittyma.java
@@ -0,0 +1,17 @@
+package GoPeli.kayttoliittyma;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Kayttoliittyma extends Application {
+
+    @Override
+    public void start(Stage ikkuna) {
+        ikkuna.setTitle("Hei Maailma!");
+        ikkuna.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(Kayttoliittyma.class);
+    }
+}
---
+++ GoPeli/src/main/java/GoPeli/kayttoliittyma/Main.java
@@ -0,0 +1,9 @@
+package GoPeli.kayttoliittyma;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Kayttoliittyma.main(args);
+    }
+    
+}
