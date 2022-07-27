b4c4dc085ec5b7e39f9b1a4f65de8a5738305981
Joonas <KA0Screativity@gmail.com>
2022-07-28 02:01:27 +0300

    Lisää käyttöliittymään pelilauta - ei vielä toimi suhteellisella polulla

--- GoPeli/src/main/java/GoPeli/kayttoliittyma/Kayttoliittyma.java
+++ GoPeli/src/main/java/GoPeli/kayttoliittyma/Kayttoliittyma.java
@@ -2,12 +2,27 @@ package GoPeli.kayttoliittyma;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class Kayttoliittyma extends Application {
 
     @Override
     public void start(Stage ikkuna) {
-        ikkuna.setTitle("Hei Maailma!");
+        Group root = new Group();
+        Scene scene = new Scene(root, 500, 500);
+        
+        Image pelilauta = new Image("src\\main\\kuvat\\Board.png");
+        ImageView imageView = new ImageView(pelilauta);
+        
+        imageView.setX(0.0);
+        imageView.setY(0.0);
+        
+        root.getChildren().add(imageView);
+        
+        ikkuna.setScene(scene);
         ikkuna.show();
     }
     
