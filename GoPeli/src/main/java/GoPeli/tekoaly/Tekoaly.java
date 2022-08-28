69f739991fb4f60f8272729b2265a326f18f63f4
Joonas Ylönen <KA0Screativity@gmail.com>
2022-08-29 00:35:03 +0300

    Kirjoita tekoälylle metodi simuloiSattumanvarainenPeli

--- GoPeli/src/main/java/GoPeli/tekoaly/Tekoaly.java
+++ GoPeli/src/main/java/GoPeli/tekoaly/Tekoaly.java
@@ -1,8 +1,11 @@
 package GoPeli.tekoaly;
 
+import GoPeli.logiikka.Koordinaatti;
 import GoPeli.logiikka.Pelitilanne;
+import GoPeli.logiikka.Ryhma;
 import GoPeli.logiikka.Siirto;
 import GoPeli.logiikka.Vari;
+import java.util.HashSet;
 import java.util.Random;
 
 public class Tekoaly {
@@ -79,8 +82,141 @@ public class Tekoaly {
         return parasLapsi;
     }
     
-    private Vari simuloiSattumanvarainenPeli(Pelitilanne lahtotilanne) {
-        // TODO
-        return Vari.MUSTA;
+    private Vari simuloiSattumanvarainenPeli(Pelitilanne tilanne) {
+        while(!tilanne.peliOhi()) {
+            Siirto[] kandidaatit = tilanne.laillisetSiirrot();
+            
+            int tyhjaIndeksi = kandidaatit.length;
+            
+            for (int indeksi = 0; indeksi < kandidaatit.length; indeksi++) {
+                if (kandidaatit[indeksi] == null) {
+                    tyhjaIndeksi = indeksi;
+                }
+            }
+            
+            while (tyhjaIndeksi > 0) {
+            
+                int sattumanvarainenIndeksi = this.random.nextInt(tyhjaIndeksi);
+                Siirto kandidaatti = kandidaatit[sattumanvarainenIndeksi];
+            
+                /*
+                    Siirto ei saa t‰ytt‰‰ omaa silm‰‰. Piste on silm‰, jos sen jokaisessa naapurissa
+                    vaaka ja pystysuunnalla on samanv‰rinen kivi, sek‰ jos piste ei ole laudan
+                    reunalla niin kolmessa nelj‰st‰ kulmittain viereisess‰ pisteess‰ on samanv‰rinen
+                    kivi, tai jos siirto on laudan reunalla, niin kaikissa kulmittain viereisiss‰
+                    pisteiss‰ on samanv‰rinen kivi.
+                */
+            
+                HashSet<Koordinaatti> naapurit = kandidaatti.getKoordinaatti().getNaapurit();
+            
+                /*
+                    Tarkastetaan pysty ja vaakasuunnan naapurit, jos jokin n‰ist‰ on tyhj‰ tai siin‰
+                    on vastustajan kivi, niin siirto ei t‰yt‰ omaa silm‰‰ ja voimme hyv‰ksy‰
+                    kandidaatin.
+                */
+                for (Koordinaatti naapuri : naapurit) {
+                    Ryhma ryhma = tilanne.getPelilauta().etsiRyhma(naapuri);
+                
+                    if (ryhma == null || ryhma.getVari() != tilanne.getPelaaja()) {
+                        tilanne.setSiirto(kandidaatti);
+                        break;
+                    }
+                }
+            
+                /*
+                    Jos edellisess‰ vaiheessa ei ole asetettu seuraavaa siirtoa (hyv‰ksytty
+                    kandidaattia) niin lis‰t‰‰n kulmittain vierekk‰iset pisteet, jotka ovat laudalla
+                    settiin.
+                */
+                HashSet<Koordinaatti> kulmaKoordinaatit = new HashSet<>();
+                
+                if (
+                    kandidaatti.getKoordinaatti().getYKoordinaatti() > 0 &&
+                    kandidaatti.getKoordinaatti().getXKoordinaatti() > 0) {
+                    
+                        kulmaKoordinaatit.add(new Koordinaatti(
+                            (byte) (kandidaatti.getKoordinaatti().getYKoordinaatti() - 1),
+                            (byte) (kandidaatti.getKoordinaatti().getXKoordinaatti() - 1)
+                    ));
+                }
+                if (
+                    kandidaatti.getKoordinaatti().getYKoordinaatti() < 8 &&
+                    kandidaatti.getKoordinaatti().getXKoordinaatti() > 0) {
+                    
+                        kulmaKoordinaatit.add(new Koordinaatti(
+                            (byte) (kandidaatti.getKoordinaatti().getYKoordinaatti() + 1),
+                            (byte) (kandidaatti.getKoordinaatti().getXKoordinaatti() - 1)
+                    ));
+                }
+                if (
+                    kandidaatti.getKoordinaatti().getYKoordinaatti() > 0 &&
+                    kandidaatti.getKoordinaatti().getXKoordinaatti() < 8) {
+                    
+                        kulmaKoordinaatit.add(new Koordinaatti(
+                            (byte) (kandidaatti.getKoordinaatti().getYKoordinaatti() - 1),
+                            (byte) (kandidaatti.getKoordinaatti().getXKoordinaatti() + 1)
+                    ));
+                }
+                if (
+                    kandidaatti.getKoordinaatti().getYKoordinaatti() < 8 &&
+                    kandidaatti.getKoordinaatti().getXKoordinaatti() < 8) {
+                    
+                        kulmaKoordinaatit.add(new Koordinaatti(
+                            (byte) (kandidaatti.getKoordinaatti().getYKoordinaatti() + 1),
+                            (byte) (kandidaatti.getKoordinaatti().getXKoordinaatti() + 1)
+                    ));
+                }
+                
+                /*
+                    Jos kulmittain vierekk‰isi‰ siirtoja on nelj‰, kolmessa niist‰ t‰ytyy olla
+                    samanv‰rinen kivi kuin pelaajan v‰ri, jotta kandidaatti on silm‰.
+                    Jos taas v‰hemm‰n, niin kaikkien t‰ytyy olla samanv‰risi‰.
+                */
+                int kulmia = kulmaKoordinaatit.size();
+                int samanvarisiaKulmanaapureita = 0;
+                
+                for (Koordinaatti kulmaNaapuri : kulmaKoordinaatit) {
+                    Ryhma ryhma = tilanne.getPelilauta().etsiRyhma(kulmaNaapuri);
+                    
+                    if (ryhma != null) {
+                        if (ryhma.getVari() == tilanne.getPelaaja()) {
+                            samanvarisiaKulmanaapureita++;
+                        }
+                    }
+                }
+                
+                if (kulmia == 4 && samanvarisiaKulmanaapureita < 3) {
+                    tilanne.setSiirto(kandidaatti);
+                    break;
+                } else if (kulmia > samanvarisiaKulmanaapureita) {
+                    tilanne.setSiirto(kandidaatti);
+                    break;
+                }
+                
+                /*
+                    Jos kandidaattia ei ole lis‰tty, niin se tarkoittaa, ett‰ se t‰ytt‰isi oman
+                    silm‰n ja se pit‰‰ poistaa kandidaattien joukosta.
+                */
+                
+                Siirto[] uusiTaulukko = new Siirto[kandidaatit.length];
+                
+                for (int indeksi = 0; indeksi < sattumanvarainenIndeksi; indeksi++) {
+                    uusiTaulukko[indeksi] = kandidaatit[indeksi];
+                }
+                
+                for (int indeksi = sattumanvarainenIndeksi + 1; indeksi < kandidaatit.length; indeksi++) {
+                    uusiTaulukko[indeksi - 1] = kandidaatit[indeksi];
+                }
+                kandidaatit = uusiTaulukko;
+                tyhjaIndeksi--;
+            }
+            
+            if (tilanne.getSiirto() == null) {
+                tilanne.setSiirto(Siirto.passaus());
+            }
+            
+            tilanne = tilanne.lisaaSiirto();
+        }
+        return tilanne.laskeVoittaja();
     }
 }
